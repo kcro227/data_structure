b7dd4d9182803a4f1f47b3516e58d7ba8dcfdbd3
kcro227 <2209352301@qq.com>
2023-05-08 22:17:13 +0800

    精简无用的代码

--- ex2/ex2.c
+++ ex2/ex2.c
@@ -2,6 +2,11 @@
 
 int arr_dat[3] = {0};
 int rmod = 0;
+/**
+ * @brief 数据扫描
+ * 
+ * @param arr 
+ */
 void data_scan(int *arr)
 {
     printf("请输入需要排序的3个数据，空格隔开：\n");
@@ -9,56 +14,35 @@ void data_scan(int *arr)
     {
         scanf("%d", &arr[i]);
     }
-    printf("递增排序输入1，递减输入0\n");
-    scanf("%d",&rmod);
 }
-void arr_rank(int *arr, int mode) // mode = 1 递增，0 递减
+/**
+ * @brief 使用冒泡法对数据进行排序
+ * 
+ * @param arr 数组名
+ */
+void arr_rank(int *arr)
 {
-    int temp = 0;
-    switch (mode)
+    int temp;
+    for (int i = 0; i < 3; i++)
     {
-    case 0:
-    {
-
-        for (int i = 0; i < 3; i++)
+        for (int j = 0; j < 3 - i - 1; j++)
         {
-            for (int j = 0; j < 3-i-1; j++)
+            if (arr[j] < arr[j + 1])
             {
-                if (arr[j] < arr[j + 1])
-                {
-                    temp = arr[j];
-                    arr[j] = arr[j + 1];
-                    arr[j + 1] = temp;
-                }
+                temp = arr[j];
+                arr[j] = arr[j + 1];
+                arr[j + 1] = temp;
             }
-            // printf("rank debug : arr[%d]=%d\n",i,arr[i]);
         }
-        break;
-    }
-    case 1:
-    {
-        for (int i = 0; i < 3; i++)
-        {
-            for (int j = 0; j < 3-i-1; j++)
-            {
-                if (arr[j] > arr[j + 1])
-                {
-                    temp = arr[j];
-                    arr[j] = arr[j + 1];
-                    arr[j + 1] = temp;
-                }
-            }
-            // printf("rank debug : arr[%d]=%d\n",i,arr[i]);
-        }
-        break;
-    }
     }
 }
 
 int main()
 {
-    data_scan(arr_dat);
-    arr_rank(arr_dat,rmod );
+    data_scan(arr_dat); // 数据扫描
+    arr_rank(arr_dat); // 排序
+    // 输出
+    printf("数据由大到小排序：\n");
     for (int i = 0; i < 3; i++)
     {
         printf("%d ", arr_dat[i]);
