de518eb74fcb7ae97a6708fd0a5debf28ca38e6e
kcro227 <2209352301@qq.com>
2023-05-22 19:54:26 +0800

    Update main.c

--- unit2/ex2/main.c
+++ unit2/ex2/main.c
@@ -4,16 +4,53 @@ typedef struct Node
 {
     int data;
     struct Node *Next;
-}Node,*LinkList;
-
-LinkList CreatList(LinkList L)
+} Node;
+Node *head = NULL;
+Node *prev = NULL;
+int LinkListInit(Node *L)
 {
-    Node *r,*s;
-    int c,i;
+    L = (Node *)malloc(sizeof(Node));
+    L ->Next = prev;
+}
+int LinkListInsert(Node *L, int data)
+{
+    Node *current;
     
+    current = (Node *)malloc(sizeof(Node));
+    if (L == NULL)
+    {
+        L = (Node *)malloc(sizeof(Node));
+        L->Next = current;
+        printf("1\n");
+    }
+    else
+    {
+        current->data = data;
+        prev = current;
+        printf("2\n");
+    }
+    current->Next = NULL;
+    prev = current;
 }
 int main()
 {
 
+    // int tempdata, i = 0;
+    int tempdata;
+    char flag;
+    printf("please input data:\n");
+    while (flag != '\n')
+    {
+        scanf("%d%c", &tempdata, &flag);
+        LinkListInsert(head, tempdata);
+        // i++;
+    }
+    Node *current = head;
+
+    while (current != NULL)
+    {
+        printf("%d", current->data);
+        current = current->Next;
+    }
     return 0;
 }
