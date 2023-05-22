7701e02cd7a6b87716a8fe592c3d2bfff479bbfc
kcro227 <2209352301@qq.com>
2023-05-22 12:49:29 +0800

    题目2

---
+++ unit2/ex2/main.c
@@ -0,0 +1,19 @@
+#include "stdio.h"
+#include "stdlib.h"
+typedef struct Node
+{
+    int data;
+    struct Node *Next;
+}Node,*LinkList;
+
+LinkList CreatList(LinkList L)
+{
+    Node *r,*s;
+    int c,i;
+    
+}
+int main()
+{
+
+    return 0;
+}
