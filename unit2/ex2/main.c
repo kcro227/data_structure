ddacddad053b7008da40c14eb591e83236bfb9ce
kcro227 <2209352301@qq.com>
2023-05-22 23:09:30 +0800

    Update main.c

--- unit2/ex2/main.c
+++ unit2/ex2/main.c
@@ -7,50 +7,111 @@ typedef struct Node
 } Node;
 Node *head = NULL;
 Node *prev = NULL;
-int LinkListInit(Node *L)
+/**
+ * @brief 从链表尾部插入数据
+ *
+ * @param data 数据
+ * @return 执行状态
+ */
+int LinkListInsert(int data)
 {
-    L = (Node *)malloc(sizeof(Node));
-    L ->Next = prev;
-}
-int LinkListInsert(Node *L, int data)
-{
+    /*创建新节点*/
     Node *current;
-    
     current = (Node *)malloc(sizeof(Node));
-    if (L == NULL)
+    current->data = data;
+    current->Next = NULL;
+
+    if (head == NULL)
     {
-        L = (Node *)malloc(sizeof(Node));
-        L->Next = current;
-        printf("1\n");
+        head = current;
     }
     else
     {
-        current->data = data;
-        prev = current;
-        printf("2\n");
+        prev->Next = current;
     }
-    current->Next = NULL;
     prev = current;
+    // printf("creat list\n");
+    return 0;
+}
+
+/**
+ * @brief 获取链表最大数据
+ * @param listpos 用于保存最大数据节点位置
+ * @return 执行状态
+ */
+int GetListDataMax(int *listpos)
+{
+    int num = 0;
+    int max = 0;
+    int pos = 0;
+    Node *nowlist;
+    Node *nowmax;
+    nowlist = head;
+    while (nowlist != NULL)
+    {
+        num = nowlist->data;
+
+        if (num > max)
+        {
+            max = num;
+            *listpos = pos;
+        }
+
+        nowlist = nowlist->Next;
+        pos++;
+    }
+    return 0;
+}
+/**
+ * @brief 删除指定位置的节点
+ * 
+ * @param pos 位置
+ * @return 运行状态
+ */
+int DelListDataMax(int pos)
+{
+    Node *p, *r;
+    int i = 0;
+    p = head;
+    while (p->Next != NULL && i < pos-1)
+    {
+        p = p->Next;
+        i++;
+    }
+    if (i != pos-1)
+        return 1;
+    r = p->Next;
+    p->Next = p->Next->Next;
+    free(r);
+    return 0;
 }
 int main()
 {
 
-    // int tempdata, i = 0;
     int tempdata;
     char flag;
     printf("please input data:\n");
     while (flag != '\n')
     {
         scanf("%d%c", &tempdata, &flag);
-        LinkListInsert(head, tempdata);
-        // i++;
+        LinkListInsert(tempdata);
     }
     Node *current = head;
 
     while (current != NULL)
     {
-        printf("%d", current->data);
+        printf("before data=%d\n", current->data);
         current = current->Next;
     }
-    return 0;
+
+    int maxlist;
+    GetListDataMax(&maxlist);
+    int code = DelListDataMax(maxlist);
+    current = head;
+    while (current != NULL)
+    {
+        printf("now data=%d\n", current->data);
+        current = current->Next;
+    }
+    return code;
 }
